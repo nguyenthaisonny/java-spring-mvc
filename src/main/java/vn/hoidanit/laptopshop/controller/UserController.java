f9f085b74eb1d89d95a9a095e783e5262fb48bcb
Nguyễn Thái Son Ny <134358623+nguyenthaisonny@users.noreply.github.com>
2025-02-11 13:18:11 +1100

    v-59

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -3,9 +3,12 @@ package vn.hoidanit.laptopshop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
 // @RestController()
@@ -34,6 +37,19 @@ public class UserController {
     public String getHomePage(Model model) {
         String text = userService.handleHello();
         model.addAttribute("sonny", text);
+        return "sonny";
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUser(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUser(Model model, @ModelAttribute("newUser") User son) {
+        System.err.println(son);
+        System.err.println("Run");
         return "sonny";
     }
 }
